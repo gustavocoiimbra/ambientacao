0a8c255863631231a44573995c91f70b35c90e5e
Gustavo Coimbra <gustavocoimbra@discente.ufg.br>
2023-04-12 20:10:45 -0300

    criacao do toolbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatSlideToggleModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    BrowserAnimationsModule, 
+    MatToolbarModule  
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
